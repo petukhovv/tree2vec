964305043ff4ad96453bb5520b3c505c32a29a68
Victor Petukhov <i@victor.am>
2018-01-31 22:47:11 +0300

    Features list var renamed

--- feature_extractor.py
+++ feature_extractor.py
@@ -7,7 +7,7 @@ from .lib.FeatureExtraction.FeatureExtractor import FeatureExtractor
 def feature_extractor(ast_file, features, output):
     root = AstReader.read(ast_file)
     features_extractor = FeatureExtractor(root, features)
-    features = features_extractor.extract()
+    features_extracted = features_extractor.extract()
 
     with open(output, 'w') as f:
-        f.write(json.dumps(features, default=str))
+        f.write(json.dumps(features_extracted, default=str))
